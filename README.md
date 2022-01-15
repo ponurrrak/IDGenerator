6aaf33209231f695de29219352860e3d2f22a2fe
Maciek Mielczarek <maciejka-67@o2.pl>
2022-01-15 11:37:38 +0100

    Add index.js

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# IDGenerator
+# idGenerator
---
+++ index.js
@@ -0,0 +1,11 @@
+const idGenerator = (idLength) => {
+  let id = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charsAmount = characters.length;
+    for(let i = 0; i < idLength; i++) {
+        id += characters.charAt(Math.floor(Math.random() * charsAmount));
+    }
+    return id;
+};
+
+module.exports = idGenerator;
